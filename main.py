272ddba320c31af1da69f3cdc5c03252b058f6c4
Den Kasyanov <den@den.ai>
2022-07-05 02:03:10 +0300

    Load variables from env file

--- main.py
+++ main.py
@@ -3,65 +3,78 @@ import os
 
 import requests
 import telegram
+from dotenv import load_dotenv
 
-DVMN_API_TOKEN = os.getenv("DVMN_API_TOKEN")
+from tg_reviews_bot.logs import TelegramLogHandler
+
+load_dotenv()
 
 TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 TG_BOT_OWNER_CHAT_ID = os.getenv("TG_BOT_OWNER_CHAT_ID")
 
+
 bot = telegram.Bot(token=TG_BOT_TOKEN)
 
-# `timestamp` нужно передавть в любом случае.
-params = {"timesamp": None}
+logging.basicConfig(handlers=[TelegramLogHandler(tg_bot=bot)])
+
+DVMN_API_TOKEN = os.getenv("DVMN_API_TOKEN")
 
 logging.warning("Бот запущен")
 
+
+params = {"timesamp": None}
+
+two_exceptions = 2
+
 while True:
+
     try:
-        response = requests.get(
-            "https://dvmn.org/api/long_polling/",
-            headers={"Authorization": f"Token {DVMN_API_TOKEN}"},
-            params=params,
-            timeout=5,
-        )
-
-    except (requests.ReadTimeout, requests.ConnectionError):
-        continue
-
-    if not response.ok:
-        continue
-
-    reviews_details = response.json()
-
-    status = reviews_details.get("status")
-
-    if status == "timeout":
-        params["timestamp"] = reviews_details.get("timestamp_to_request")
-
-    elif status == "found":
-        params["timestamp"] = reviews_details.get("last_attempt_timestamp")
-
-        # Assuming multiple reviews cannot be posted at the exact same moment
-        latest_attempt = reviews_details["new_attempts"][0]
-        if latest_attempt:
-
-            lesson_title = latest_attempt["lesson_title"]
-            lesson_url = latest_attempt["lesson_url"]
-
-            text_header = (
-                f'У вас проверили работу <a href="{lesson_url}">"{lesson_title}"</a>'
+        try:
+            response = requests.get(
+                "https://dvmn.org/api/long_polling/",
+                headers={"Authorization": f"Token {DVMN_API_TOKEN}"},
+                params=params,
+                timeout=5,
             )
 
-            is_successful = not latest_attempt["is_negative"]
+        except (requests.ReadTimeout, requests.ConnectionError):
+            continue
 
-            text_review_result = (
-                "Преподавателю все понравилось, можно приступать к следующему уроку."
-                if is_successful
-                else "К сожалению, в работе нашлись ошибки."
-            )
+        if not response.ok:
+            continue
 
-            bot.send_message(
-                text=f"{text_header}\n\n{text_review_result}",
-                chat_id=TG_BOT_OWNER_CHAT_ID,
-                parse_mode=telegram.ParseMode.HTML,
-            )
+        reviews_details = response.json()
+
+        status = reviews_details.get("status")
+
+        if status == "timeout":
+            params["timestamp"] = reviews_details.get("timestamp_to_request")
+
+        elif status == "found":
+            params["timestamp"] = reviews_details.get("last_attempt_timestamp")
+
+            # Assuming multiple reviews cannot be posted at the exact same moment
+            latest_attempt = reviews_details["new_attempts"][0]
+            if latest_attempt:
+
+                lesson_title = latest_attempt["lesson_title"]
+                lesson_url = latest_attempt["lesson_url"]
+
+                text_header = f'У вас проверили работу <a href="{lesson_url}">"{lesson_title}"</a>'
+
+                is_successful = not latest_attempt["is_negative"]
+
+                text_review_result = (
+                    "Преподавателю все понравилось, можно приступать к следующему уроку."
+                    if is_successful
+                    else "К сожалению, в работе нашлись ошибки."
+                )
+
+                bot.send_message(
+                    text=f"{text_header}\n\n{text_review_result}",
+                    chat_id=TG_BOT_OWNER_CHAT_ID,
+                    parse_mode=telegram.ParseMode.HTML,
+                )
+
+    except Exception:
+        logging.exception("Бот упал с ошибкой")
